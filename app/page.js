1e3ceef2a8673730074c0b95a061c38646974b38
yehia0023 <yehiam078@gmail.com>
2023-05-16 21:05:33 +0200

    creating pages

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <h1 className="text-7xl">Hello World</h1>
+    <div className="bg-indigo-800 text-white flex px-10">
+      <Link className="p-2.5 hover:bg-red-600 mr-auto" href="/accounts">Accounts</Link>
+      <Link className="p-2.5 hover:bg-red-600" href="/login">Login</Link>
+      <Link className="p-2.5 hover:bg-red-600" href="/signup">Sign up</Link>
+    </div>
   )
 }
